2d6e122a78c3603e83474ebabf203eb58eea9a56
Augusto Tomasi <augustopreisthomasi@gmail.com>
2024-11-01 15:06:07 -0300

    ajustes na validacao de categoria de nota

--- src/main/java/com/satc/todolist/dtos/CategoriaNotaCadastroDTO.java
+++ src/main/java/com/satc/todolist/dtos/CategoriaNotaCadastroDTO.java
@@ -1,7 +1,8 @@
 package com.satc.todolist.dtos;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record CategoriaNotaCadastroDTO(
-    @NotNull(message = "A descrição da categoria de nota não pode ser nula") String descricao) {
+    @NotNull(message = "A descrição da categoria de nota não pode ser nula") @Size(min = 1, max = 50, message = "A descrição da categoria deve ter entre {min} e {max} caracteres") String descricao) {
 }
